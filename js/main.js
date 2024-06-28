db49e028c9151c917dea2cbc4bd28e3916b971c9
Makeilokoogehena <studworkse@gmail.com>
2024-06-28 23:14:46 +0300

    FEAT: create logic for buttons on main page, create game page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+const buttonVegetables = document.querySelector(".menu__link_first");
+const buttonCountries = document.querySelector(".menu__link_second");
+const buttonFruits = document.querySelector(".menu__link_third");
 
+function onButtonVegetablesClick() {
+  buttonVegetables.classList.add("menu__link_first-open");
+  window.location.assign("gamepage.html");
+}
+
+function onButtonCountriesClick() {
+  buttonCountries.classList.add("menu__link_second-open");
+  window.location.assign("gamepage.html");
+}
+
+function onButtonFruitsClick() {
+  buttonFruits.classList.add("menu__link_third-open");
+  window.location.assign("gamepage.html");
+}
+
+buttonVegetables.addEventListener("click", onButtonVegetablesClick);
+buttonCountries.addEventListener("click", onButtonCountriesClick);
+buttonFruits.addEventListener("click", onButtonFruitsClick);
