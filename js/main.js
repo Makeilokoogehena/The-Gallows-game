72ba665a0c87e36715d018adcb130c4406b3dae2
Makeilokoogehena <studworkse@gmail.com>
2024-07-08 23:00:39 +0300

    REFACTOR: overwrite HTML and CSS components and logic for main page buttons. Create logic for game title

--- js/main.js
+++ js/main.js
@@ -1,22 +1,37 @@
-const buttonVegetables = document.querySelector(".menu__link_first");
-const buttonCountries = document.querySelector(".menu__link_second");
-const buttonFruits = document.querySelector(".menu__link_third");
+const vegetablesButton = document.querySelector(".menu-main__link_first");
+const countriesButton = document.querySelector(".menu-main__link_second");
+const fruitsButton = document.querySelector(".menu-main__link_third");
+const mainMenu = document.querySelector(".menu-main");
+const gameMenu = document.querySelector(".game-menu");
+const gameMenuWrapperTitle = document.querySelector(
+  ".game-menu__wrapper-title"
+);
+const topicsArray = [
+  { option: "Vegetables", id: 1 },
+  { option: "Countries", id: 2 },
+  { option: "Fruits", id: 3 },
+];
 
-function onButtonVegetablesClick() {
-  buttonVegetables.classList.add("menu__link_first-open");
-  window.location.assign("gamepage.html");
+const createGameTitle = (array) => {
+  const title = document.createElement("h1");
+  title.classList.add("game-menu__title");
+  title.innerHTML = `Topic: ${array}`;
+  return title;
+};
+
+function openGameMenu() {
+  mainMenu.style.display = "none";
+  gameMenu.style.display = "block";
 }
 
-function onButtonCountriesClick() {
-  buttonCountries.classList.add("menu__link_second-open");
-  window.location.assign("gamepage.html");
+function onClickTopicsButton(e) {
+  openGameMenu();
+  const item = createGameTitle(e.target.innerHTML);
+  gameMenuWrapperTitle.appendChild(item);
 }
 
-function onButtonFruitsClick() {
-  buttonFruits.classList.add("menu__link_third-open");
-  window.location.assign("gamepage.html");
-}
+//
 
-buttonVegetables.addEventListener("click", onButtonVegetablesClick);
-buttonCountries.addEventListener("click", onButtonCountriesClick);
-buttonFruits.addEventListener("click", onButtonFruitsClick);
+vegetablesButton?.addEventListener("click", (e) => onClickTopicsButton(e));
+countriesButton?.addEventListener("click", (e) => onClickTopicsButton(e));
+fruitsButton?.addEventListener("click", (e) => onClickTopicsButton(e));
